a85de22b504fe67e6bc2300119a9d4385ea6b1fe
jordi <jordivandooren@gmail.com>
2019-12-28 14:27:33 +0100

    Add offlineimap config.

---
+++ .offlineimap.py
@@ -0,0 +1,23 @@
+from subprocess import check_output
+
+
+def get_pass():
+    password = check_output("pass show website/google.com", shell=True)
+    return password
+
+
+gmail_prefix = "[Gmail]/"
+
+
+def to_local_name(name):
+    if name.startswith(gmail_prefix):
+        name = name.replace(gmail_prefix, "")
+    return name.replace(" ", "_")
+
+
+def to_remote_name(name):
+    gmail_defaults = ["All Mail", "Bin", "Drafts", "Important",
+                      "Sent Mail", "Spam", "Starred"]
+    if name.replace("_", " ") in gmail_defaults:
+        name = gmail_prefix + name
+    return name.replace("_", " ")
